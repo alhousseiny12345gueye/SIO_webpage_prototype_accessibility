1b7ad8bb6abf60b4c9578adea416469966446a26
Alhousseiny Gueye <s371961@oslomet.no>
2023-12-27 18:42:45 +0000

    Update server.js

--- server.js
+++ server.js
@@ -7,10 +7,7 @@ const app = express();
 // Specify the port to listen on
 const port = 80;
 
-// Define a route for the root path
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/app/index.html'));
-});
+
 
 // Start the server and listen on the specified port
 app.listen(port, () => {
